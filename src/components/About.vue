35d54394acfadcb6ee00d89c228fcf2c4d0da917
awguitar86 <awguitar86@gmail.com>
2019-11-26 11:01:40 -0700

    added new pictures, and added content to About component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,24 @@
 <template>
     <v-container class="d-flex flex-column align-center mt-5">
-        <h1 class="headline text-uppercase">About Component</h1>
+        <h1 class="headline text-uppercase mb-3 text-center">Welcome to the Living Planet Events!</h1>
+        <img src="../assets/food-platters.jpg" alt="food plater" id="food-platter-img" class="mt-3 mb-5"/>
+        <p>We are dedicated to make your special occasion everything you could want and more. We offer a full service experience that will leave you and your guests more than satisfied. We provide a menu that is custom to you and your wants and needs, created at the hands of our Executive Chef, Dave Prows. We pride ourselves in quality and outstanding customer service. Contact us today to plan your next event!</p>
+        
+        <img src="../assets/foodBySharks.jpg" alt="food by sharks" id="food-by-sharks-img" class="mt-3 mb-5"/>
+        <p>Our creators, owners, chefs and managers have been in the catering and events business for many years. With over 40 years experience with multiple corporations we bring all the knowlege and professionalism you won’t find anywhere else.</p>
+    
     </v-container>
 </template>
 
+<style lang="scss" scoped>
+    #food-platter-img {
+        width: 100%;
+        height: auto;
+        border-radius: 5px;
+    }
+    #food-by-sharks-img {
+        width: 100%;
+        height: auto;
+        border-radius: 5px;
+    }
+</style>
